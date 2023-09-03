615884060ddb4c5ac929e101bb9bad5e1b083ebb
NirajShr3stha <nirajshrestha@ismt.edu.np>
2023-09-03 15:11:34 +0545

    commented in cache.dart

--- lib/views/caching_data/cache.dart
+++ lib/views/caching_data/cache.dart
@@ -1,25 +1,25 @@
-private void saveImageToInternalStorage(Bitmap bitmap) {
-    // Get the context wrapper
-    ContextWrapper cw = new ContextWrapper(getApplicationContext());
+// private void saveImageToInternalStorage(Bitmap bitmap) {
+//     // Get the context wrapper
+//     ContextWrapper cw = new ContextWrapper(getApplicationContext());
 
-    // Create a directory for your app's private pictures
-    File directory = cw.getDir("imageDir", Context.MODE_PRIVATE);
+//     // Create a directory for your app's private pictures
+//     File directory = cw.getDir("imageDir", Context.MODE_PRIVATE);
 
-    // Create the image file
-    File mypath = new File(directory, "myImage.jpg");
+//     // Create the image file
+//     File mypath = new File(directory, "myImage.jpg");
 
-    FileOutputStream fos = null;
-    try {
-        fos = new FileOutputStream(mypath);
-        // Use the compress method on the Bitmap object to write image to the OutputStream
-        bitmap.compress(Bitmap.CompressFormat.PNG, 100, fos);
-    } catch (Exception e) {
-        e.printStackTrace();
-    } finally {
-        try {
-            fos.close();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-    }
-}
+//     FileOutputStream fos = null;
+//     try {
+//         fos = new FileOutputStream(mypath);
+//         // Use the compress method on the Bitmap object to write image to the OutputStream
+//         bitmap.compress(Bitmap.CompressFormat.PNG, 100, fos);
+//     } catch (Exception e) {
+//         e.printStackTrace();
+//     } finally {
+//         try {
+//             fos.close();
+//         } catch (IOException e) {
+//             e.printStackTrace();
+//         }
+//     }
+// }
